a215997a37a7c8580bee26e98497c914e19a67d2
anisazade <116896458+anisazade@users.noreply.github.com>
2025-10-22 22:36:22 +0330

    chore: add initial static React components and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,117 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react';
+// import { use } from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function SerachBar() {
+    return (
+        <>
+            <button id="search-button">search</button>
+            <input type="Search repo, or a GitHub profile" id="search-input" />
+        </>
+    );
 }
 
-export default App
+function UserInformation({
+    username,
+    profile_picture_url,
+    bio,
+    follower_count,
+    following_count,
+    location,
+}) {
+    return (
+        <div>
+            <div>
+                <h2>{username}</h2>
+                <p>{bio}</p>
+            </div>
+            <div>
+                <img src={profile_picture_url} alt="Profile Picture" />
+                <p>Followers {follower_count}</p>
+                <p>Following {following_count}</p>
+                <p>Location {location}</p>
+            </div>
+        </div>
+    );
+}
+
+function RepositoryGrid({ repository_list }) {
+    const reposJSX = repository_list.map((repo) => {
+        return <Repository key={repo.id} repository={repo} />;
+    });
+
+    return <div id="repository_grid">{reposJSX}</div>;
+}
+
+function Repository({ repository }) {
+    return (
+        <div>
+            <a href={repository.URL}>repo link</a>
+            <div>
+                <p>{repository.title}</p>
+                <p>{repository.description}</p>
+            </div>
+            <div>
+                <p>{repository.licence_type}</p>
+                <p>{repository.forks}</p>
+                <p>{repository.stars}</p>
+                <p>{repository.latest_update}</p>
+            </div>
+        </div>
+    );
+}
+
+function Header() {
+    return (
+        <div>
+            <SerachBar />
+        </div>
+    );
+}
+
+function Profile({ data }) {
+    return (
+        <div id="profile-container">
+            <UserInformation
+                username={data.username}
+                profile_picture_url={data.profilePicture}
+                bio={data.bio}
+                follower_count={data.followers}
+                following_count={data.followings}
+                location={data.location}
+            />
+            <RepositoryGrid repository_list={data.repos} />
+            <a href="">View all rpositories</a>
+        </div>
+    );
+}
+
+function App() {
+    return (
+        <>
+            <Header />
+            <Profile data={Data} />
+        </>
+    );
+}
+
+const Data = {
+    username: 'GitHuB',
+    bio: 'How people build software.',
+    followers: 27839,
+    followings: 0,
+    location: 'San Francisco, CA',
+    repos: [
+        {
+            title: '.github',
+            description: 'Community Health Files for the @GitHub organization',
+            licence_type: null,
+            forks: 2369,
+            stars: 703,
+            latest_update: '4 days ago',
+            URL: ""
+        },
+    ],
+};
+
+export default App;
