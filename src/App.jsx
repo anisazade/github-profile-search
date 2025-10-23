18753612aeab25ec6104f6080a05ae2e041b9a73
anisazade <116896458+anisazade@users.noreply.github.com>
2025-10-24 00:40:57 +0330

    chore: Add API profile fecth

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // import { use } from 'react';
 import './App.css';
 
@@ -11,25 +11,18 @@ function SerachBar() {
     );
 }
 
-function UserInformation({
-    username,
-    profile_picture_url,
-    bio,
-    follower_count,
-    following_count,
-    location,
-}) {
+function UserInformation({ user }) {
     return (
         <div>
             <div>
-                <h2>{username}</h2>
-                <p>{bio}</p>
+                <h2>{user.username}</h2>
+                <p>{user.bio}</p>
             </div>
             <div>
-                <img src={profile_picture_url} alt="Profile Picture" />
-                <p>Followers {follower_count}</p>
-                <p>Following {following_count}</p>
-                <p>Location {location}</p>
+                <img src={user.avatar_url} alt="Profile Picture" />
+                <p>Followers {user.followers}</p>
+                <p>Following {user.following}</p>
+                <p>Location {user.location}</p>
             </div>
         </div>
     );
@@ -37,7 +30,7 @@ function UserInformation({
 
 function RepositoryGrid({ repository_list }) {
     const reposJSX = repository_list.map((repo) => {
-        return <Repository key={repo.id} repository={repo} />;
+        return <Repository key={repo.title} repository={repo} />;
     });
 
     return <div id="repository_grid">{reposJSX}</div>;
@@ -46,7 +39,7 @@ function RepositoryGrid({ repository_list }) {
 function Repository({ repository }) {
     return (
         <div>
-            <a href={repository.URL}>repo link</a>
+            <a href={repository.url}>repo link</a>
             <div>
                 <p>{repository.title}</p>
                 <p>{repository.description}</p>
@@ -55,7 +48,7 @@ function Repository({ repository }) {
                 <p>{repository.licence_type}</p>
                 <p>{repository.forks}</p>
                 <p>{repository.stars}</p>
-                <p>{repository.latest_update}</p>
+                <p>{repository.last_modified}</p>
             </div>
         </div>
     );
@@ -70,48 +63,66 @@ function Header() {
 }
 
 function Profile({ data }) {
-    return (
-        <div id="profile-container">
-            <UserInformation
-                username={data.username}
-                profile_picture_url={data.profilePicture}
-                bio={data.bio}
-                follower_count={data.followers}
-                following_count={data.followings}
-                location={data.location}
-            />
-            <RepositoryGrid repository_list={data.repos} />
-            <a href="">View all rpositories</a>
-        </div>
-    );
+    if(data){
+        return (
+            <div id="profile-container">
+                <UserInformation user={data.user} />
+                <RepositoryGrid repository_list={data.repos} />
+                <a href="">View all rpositories</a>
+            </div>
+        );
+    }
 }
 
 function App() {
+    const [profile, setProfile] = useState(null);
+    function fetch_data() {
+        getProfile('lukastaegert').then(setProfile);
+    }
     return (
         <>
             <Header />
-            <Profile data={Data} />
+            <Profile data={profile} />
+            {(!profile) && <button onClick={fetch_data}>fecth a profile</button>}
         </>
     );
 }
 
-const Data = {
-    username: 'GitHuB',
-    bio: 'How people build software.',
-    followers: 27839,
-    followings: 0,
-    location: 'San Francisco, CA',
-    repos: [
-        {
-            title: '.github',
-            description: 'Community Health Files for the @GitHub organization',
-            licence_type: null,
-            forks: 2369,
-            stars: 703,
-            latest_update: '4 days ago',
-            URL: ""
+async function getProfile(username) {
+    const userAPI = URL.parse(username, 'https://api.github.com/users/');
+    const userJson = await fetch(userAPI, {
+        headers: {
+            Accept: 'application/vnd.github+json',
+            'X-GitHub-Api-Version': '2022-11-28',
         },
-    ],
-};
+    })
+        .then((response) => (response.ok ? response.json() : null))
+        .catch(console.error);
+
+    if (!userJson) return {};
+
+    const reposJson = await fetch(URL.parse(userJson.repos_url)).then((data) => data.json());
+    const user = {
+        username: userJson.login,
+        avatar_url: userJson.avatar_url,
+        bio: userJson.bio,
+        followers: userJson.followers,
+        following: userJson.following,
+        location: userJson.location,
+    };
+    const repos = reposJson.map((repo) => {
+        return {
+            title: repo.name,
+            description: repo.description,
+            forks: repo.forks_count,
+            license: repo.spdxid,
+            stars: repo.stargazers_count,
+            last_modified: repo.updated_at,
+            url: repo.clone_url,
+        };
+    });
+    console.log({ user, repos });
+    return { user, repos };
+}
 
 export default App;
